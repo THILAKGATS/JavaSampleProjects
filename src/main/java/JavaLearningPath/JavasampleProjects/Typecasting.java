e0a95b6bc4189f50debca629eb17c6d4a76efc65
THILAKGATS <thilakrock1799@gmail.com>
2023-11-09 18:47:39 +0530

    Type casting done

---
+++ src/main/java/JavaLearningPath/JavasampleProjects/Typecasting.java
@@ -0,0 +1,20 @@
+package JavaLearningPath.JavasampleProjects;
+
+public class Typecasting 
+{
+	public static void main(String args[])
+	{
+		//Type Casting
+		byte a = 10;
+		int n;
+		n= a;// implicit type casting
+		System.out.println(n);
+		byte s ;
+		int l = 20;
+		s= (byte)l;// explicit Type casting
+		System.out.println(l);
+		// escape sequence
+		System.out.println("Hello everyone,\n welcome you");// \n it helps to move to the next line
+		/*******there are more sequence like this *****/
+	}
+}
