e06e3cf3fabb80d37da2fc8ff890a492e03b7d48
THILAKGATS <thilakrock1799@gmail.com>
2023-11-09 18:31:06 +0530

    Variables and literals

---
+++ src/main/java/JavaLearningPath/JavasampleProjects/VariablesandLiterals.java
@@ -0,0 +1,32 @@
+/****************************************
+ * **********Variables and literals******
+ ******************************************/
+
+package JavaLearningPath.JavasampleProjects;
+
+public class VariablesandLiterals 
+{
+	public static void main(String args[])
+	{
+		
+		int ticketPrice = 800; // variable declaration 
+		System.out.println(ticketPrice);
+		ticketPrice = 1000;
+		System.out.println("The ticket price is" + ticketPrice);
+		/******literals*******/
+		// the value is called as literals
+		int a = 10; //numerical literal
+		boolean value = true; // boolean literal 
+		char c = '4';// char literals
+		//hexadecimal
+		int num = 0x10;
+		float f = 187788;
+		long len = 802214880;
+		System.out.println(len);
+		System.out.println(f);
+		
+		System.out.println(num);
+		
+	}
+	
+}
